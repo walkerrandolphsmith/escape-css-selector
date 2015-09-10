2b10f46cd2b9592b552840b793cd2fb1f0f7de9a
Walker Randolph Smith <walkerrandolphsmith@users.noreply.github.com>
2015-09-09 23:17:53 -0400

    Create escape-css-filter.js

---
+++ escape-css-filter.js
@@ -0,0 +1,16 @@
+export function escapeCssSelector() {
+		var specials = [
+		  '#', '&', '~', '=', '>',
+		  "'", ':', '"', '!', ';', ','
+		];
+		var regexSpecials = [
+		  '.', '*', '+', '|', '[', ']', '(', ')', '/', '^', '$'
+		];
+		var sRE = new RegExp(
+		  '(' + specials.join('|') + '|\\' + regexSpecials.join('|\\') + ')', 'g'
+		);
+
+		return function (selector) {
+			return selector.replace(sRE, '\\$1');
+		}
+};
