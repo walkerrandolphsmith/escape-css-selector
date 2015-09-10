c85fb3c5bece3fd760ea5fe11ea29ad24c5895b1
Walker Randolph Smith <walkerrandolphsmith@users.noreply.github.com>
2015-09-09 23:18:28 -0400

    Update escape-css-filter.js
    Fix whitespace

--- escape-css-filter.js
+++ escape-css-filter.js
@@ -1,16 +1,16 @@
 export function escapeCssSelector() {
-		var specials = [
-		  '#', '&', '~', '=', '>',
-		  "'", ':', '"', '!', ';', ','
-		];
-		var regexSpecials = [
-		  '.', '*', '+', '|', '[', ']', '(', ')', '/', '^', '$'
-		];
-		var sRE = new RegExp(
-		  '(' + specials.join('|') + '|\\' + regexSpecials.join('|\\') + ')', 'g'
-		);
-
-		return function (selector) {
-			return selector.replace(sRE, '\\$1');
-		}
+	var specials = [
+	  '#', '&', '~', '=', '>',
+	  "'", ':', '"', '!', ';', ','
+	];
+	var regexSpecials = [
+	  '.', '*', '+', '|', '[', ']', '(', ')', '/', '^', '$'
+	];
+	var sRE = new RegExp(
+	  '(' + specials.join('|') + '|\\' + regexSpecials.join('|\\') + ')', 'g'
+	);
+	
+	return function (selector) {
+		return selector.replace(sRE, '\\$1');
+	}
 };
