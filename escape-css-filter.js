200cd287abb6d6a176c1cfb1815113febb9565b1
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-07-07 09:28:07 -0400

    condense return statement. Oneline the special characters list. export default instead of exporting a named function.

--- escape-css-filter.js
+++ escape-css-filter.js
@@ -1,16 +1,12 @@
-export function escapeCssSelector() {
-	var specials = [
-	  '#', '&', '~', '=', '>',
-	  "'", ':', '"', '!', ';', ','
-	];
-	var regexSpecials = [
-	  '.', '*', '+', '|', '[', ']', '(', ')', '/', '^', '$'
-	];
-	var sRE = new RegExp(
-	  '(' + specials.join('|') + '|\\' + regexSpecials.join('|\\') + ')', 'g'
-	);
-	
-	return function (selector) {
-		return selector.replace(sRE, '\\$1');
-	}
+export default () => {
+    var specials = [
+      '#', '&', '~', '=', '>', "'", ':', '"', '!', ';', ','
+    ];
+    var regexSpecials = [
+      '.', '*', '+', '|', '[', ']', '(', ')', '/', '^', '$'
+    ];
+    var sRE = new RegExp(
+      '(' + specials.join('|') + '|\\' + regexSpecials.join('|\\') + ')', 'g'
+    );
+    return selector => selector.replace(sRE, '\\$1');
 };
